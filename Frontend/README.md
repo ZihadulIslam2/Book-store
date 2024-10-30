40f0279d8fe2b485dcd1db563136c06b3aac89bc
zihadulislam2 <zihadul708@gmail.com>
2024-10-30 22:56:53 +0600

    BookCard

--- Frontend/README.md
+++ Frontend/README.md
@@ -10,3 +10,5 @@
 - add a dropdown option when the user cleck to the user icon.? (1.27 h)
 - create home componetns ? 1.34 min
 - feath data from the books.json file ? (1.44 min)
+- create a dropdown category filter on top sellers.? 
+2.04h 
--- Frontend/src/pages/home/TopSelles.jsx
+++ Frontend/src/pages/home/TopSelles.jsx
@@ -1,21 +1,56 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import BookCard from "../books/BookCard";
+
+const category = [
+  "choose a genre",
+  "Business",
+  "Fiction",
+  "Horror",
+  "Adventure",
+];
 
 const TopSelles = () => {
-    const [books, setBooks] = useState([])
+  const [books, setBooks] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState("choose a genre");
 
-    useEffect(()=>{
-        fetch("books.json")
-        .then( res =>res.json())
-        .then((data) => setBooks(data))
+console.log("selectedCategory:  ",selectedCategory)
 
-    }, [])
+  useEffect(() => {
+    fetch("books.json")
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
 
-    console.log("Books data: ", books)
-    
+  console.log("Books data: ", books);
+
+  const filteredBook = selectedCategory === "choose a genre"?books : books.filter(book =>book.category === selectedCategory.toLowerCase())
+  console.log("filtered book:  " ,filteredBook)
 
   return (
-    <div>TopSelles</div>
-  )
-}
+    <div className="py-10">
+      <h2 className="text-3xl font-semibold mb-6 "> Top Sellers</h2>
+      {/* caragory filter */}
 
-export default TopSelles
+      <div className="mb-8 flex items-center">
+        <select 
+        onChange={(e)=>(setSelectedCategory(e.target.value))}
+        name="category" id="category" className="border bg-[#EAEAEA] border-gray-300 rounded-md px-4 py-2 focus:outline-none">
+
+          {category.map((categoryData, index) => (
+            <option value={categoryData} key={index}>
+              {categoryData}
+            </option>
+            
+          ))}
+        </select>
+      </div>
+      {
+        filteredBook.map((filteredBookData,index)=>(
+          <BookCard key={index} book={filteredBookData}/>
+        ))
+      }
+    </div>
+  );
+};
+
+export default TopSelles;
