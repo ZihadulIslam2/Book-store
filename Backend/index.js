85a7d3e8e66c8f9910b3ceb4c1abf62da74a4342
zihadulIslam2 <zihadul708@gmail.com>
2024-12-27 12:26:04 +0600

    Backend api for admin

--- Backend/index.js
+++ Backend/index.js
@@ -40,6 +40,11 @@ app.use("/api/orders", orderRoutes )
 const userRoutes = require("./src/users/user.route")
 app.use('/api/auth', userRoutes)
 
+const adminRoutes = require("./src/stats/admin.stats")
+app.use('/api/admin', adminRoutes)
+
+
+
   
 async function main() {
   await mongoose.connect(process.env.DB_URL);
